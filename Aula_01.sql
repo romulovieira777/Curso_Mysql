18e5114c8b5e4f00cd3fa94b99887280adad2cbc
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-06-08 12:11:19 -0300

    Aula_01
    - Criando um Banco de Dados
    - Verificando os Banco de Dados no Servidor
    - Escolhendo um Banco de Dados
    - Excluindo um Banco de Dados
    - Verificando as Tabelas no Banco de Dados

---
+++ Aula_01.sql
@@ -0,0 +1,10 @@
+/* Criando um Banco de Dados */
+CREATE DATABASE
+	db_Biblioteca;
+
+/* Verificando os Banco de Dados no Servidor */
+SHOW DATABASES;
+
+/* Escolhendo um Banco de Dados */
+USE
+	db_Biblioteca;
