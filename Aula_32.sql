ada8ae4771c9718c5b84a867ecf7136807db1c2b
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-06-22 12:23:13 -0300

    Mostrando a Estrutura de Tabelas
    - Escolhendo um Banco de Dados.
    - Mostrando os Banco de Dados.
    - Mostrando as Tabelas.
    - Mostrando Todos os Dados da Tabela.

---
+++ Aula_32.sql
@@ -0,0 +1,21 @@
+/* Escolhendo um Banco de Dados */
+USE
+	db_biblioteca;
+    
+    
+/* Mostrando os Banco de Dados */
+SHOW DATABASES;
+
+
+/* Mostrando as Tabelas */
+SHOW TABLES;
+
+
+/* Mostrando as Colunas da Tabela */
+SHOW COLUMNS FROM
+	tbl_livro;
+    
+    
+/* Mostrando Todos os Dados da Tabela */
+DESC
+	tbl_livro;
